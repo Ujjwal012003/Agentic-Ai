995f9d86df012b1feb4488225236cd2d3f21b5c5
Ujjwal Bhardwaj <ujjwaal05@gmail.com>
2025-08-31 21:46:59 +0530

    main.py

--- Debating_Agent/crew/debate/src/debate/main.py
+++ Debating_Agent/crew/debate/src/debate/main.py
@@ -18,7 +18,7 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': "India's Economic Growth Strategy",
+        'topic': "ADD YOUR TOPIC AT WHICH DEBATE GONNA TAKE PLACE",#WRITE DOWN YOUR TOPIC ON WHICH YOU WANT THE DEBATE TO HAPPEN
         
     }
     
@@ -67,3 +67,4 @@ def test():
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
