c798d0fc60ffe2cfdecb073346e265320ff120c1
Ujjwal Bhardwaj <ujjwaal05@gmail.com>
2025-08-31 21:49:49 +0530

    Update crew.py

--- Debating_Agent/crew/debate/src/debate/crew.py
+++ Debating_Agent/crew/debate/src/debate/crew.py
@@ -2,9 +2,6 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
 class Debate():
@@ -13,59 +10,49 @@ class Debate():
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
     def debater(self) -> Agent:
         return Agent(
-            config=self.agents_config['debater'], # type: ignore[index]
+            config=self.agents_config['debater'], # Same agent as in agents.yaml file defined
             verbose=True
         )
 
     @agent
     def judge(self) -> Agent:
         return Agent(
-            config=self.agents_config['judge'], # type: ignore[index]
+            config=self.agents_config['judge'], # Same agent as in agents.yaml file defined
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
     def propose(self) -> Task:
         return Task(
-            config=self.tasks_config['propose'], # type: ignore[index]
+            config=self.tasks_config['propose'], # Same task as in tasks.yaml file defined
         )
 
     @task
     def oppose(self) -> Task:
         return Task(
-            config=self.tasks_config['oppose'], # type: ignore[index]
+            config=self.tasks_config['oppose'], # Same task as in tasks.yaml file defined
             
         )
     
     @task
     def decide(self) -> Task:
         return Task(
-            config=self.tasks_config['decide'], # type: ignore[index]
+            config=self.tasks_config['decide'], # Same task as in tasks.yaml file defined
             
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the Debate crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
+    
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents, 
+            tasks=self.tasks, 
             process=Process.sequential,
             verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
+
